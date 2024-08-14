0df7b256f8f0845e1a221c70bbc89b304e388c66
ilyasamsonov <i.brodovoy@artvision.ai>
2024-08-14 23:37:48 +0300

    Edit: Use formatters

--- source/arg_parser/example_project/schemes/users.py
+++ source/arg_parser/example_project/schemes/users.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel, Json
 
-'''
+"""
 You have to use pydantic models for your handlers to get data from HTTP body.
 Right now you can use only basic types and pydantic Json type for your annotations in pydantic models.
 
 (!) In this version, the use of nested models for handler arguments is not supported.
-'''
+"""
 
 
 class UserModel(BaseModel):
