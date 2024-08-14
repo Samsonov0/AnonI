d73de5a27d67037dcf671679eeff672e2f44c862
ilyasamsonov <i.brodovoy@artvision.ai>
2024-08-14 23:36:26 +0300

    Edit: Use formatters

--- source/utils/fill_scheme.py
+++ source/utils/fill_scheme.py
@@ -1,15 +1,16 @@
 import json
-from typing import get_type_hints
+from typing import Any, Callable, Type, get_type_hints
 
-from pydantic import Json
+from pydantic import BaseModel, Json
+from pydantic.fields import FieldInfo
 from pydantic_core import PydanticUndefined
 
 
-async def fill_scheme(handler, body):
-    scheme = get_type_hints(handler).get('scheme')
+async def fill_scheme(handler: Callable, body: dict) -> BaseModel:
+    scheme: Type[BaseModel] = get_type_hints(handler).get("scheme")
 
     try:
-        scheme_fields = scheme.__fields__
+        scheme_fields: dict[str:FieldInfo] = scheme.__fields__
     except AttributeError:
         raise AttributeError(
             'Handler "{handler}" have to include used scheme name via annotations types'.format(
@@ -17,14 +18,18 @@ async def fill_scheme(handler, body):
             )
         )
 
-    data = {}
+    data: dict[str:Any] = dict()
     for field_name, field_info in scheme_fields.items():
-        body_data = body.get(field_name)
-        if field_info.annotation is Json and body_data is not None and isinstance(body_data, (list, dict)):
-            body_data = json.dumps(body_data)
+        body_data: Any = body.get(field_name)
+        if (
+            field_info.annotation is Json
+            and body_data is not None
+            and isinstance(body_data, (list, dict))
+        ):
+            body_data: str = json.dumps(body_data)
 
         if field_info.is_required and body_data is None:
-            default_value = field_info.default
+            default_value: Any = field_info.default
 
             if default_value is PydanticUndefined:
                 raise ValueError(
