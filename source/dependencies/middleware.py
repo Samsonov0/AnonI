9d6348900e7679a9fd5b61e68957aff3884b5f65
ilyasamsonov <i.brodovoy@artvision.ai>
2024-08-15 21:16:15 +0300

    Add: Type Hints [0.0.2]

--- source/dependencies/middleware.py
+++ source/dependencies/middleware.py
@@ -11,21 +11,21 @@ class AbstractMiddleware:
         self.request_data: RequestData = request_data
         self.response_data: ResponseData = response_data
 
-    async def __call__(self):
+    async def __call__(self) -> RequestData | ResponseData:
         return await self._processing()
 
-    async def _processing(self): ...
+    async def _processing(self) -> RequestData | ResponseData: ...
 
 
 class BeforeMiddleware(AbstractMiddleware):
-    async def _processing(self):
+    async def _processing(self) -> RequestData:
         print(self.request_data, "request")
 
         return self.request_data
 
 
 class AfterMiddleware(AbstractMiddleware):
-    async def _processing(self):
+    async def _processing(self) -> ResponseData:
         print(self.response_data, "response")
 
         return self.response_data
